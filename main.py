7592f49ef2ed260a377ea9007b1fbc93c045333e
Murat Bayraktar <59517900+muratbayrktr@users.noreply.github.com>
2021-04-05 08:16:42 -0400

    Add files via upload
    python3 / python main.py "text.txt"

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import threading
 import time
+import sys
+
+# supply the file name which should be in the same directory with command line argument enclosed with double quotes
 
 # GLOBAL VARIABLE
 result = 1
+threadlist = [4,16,32,64]
 
 # worker class in order to multiply on threads
 class Worker:
@@ -12,12 +16,13 @@ class Worker:
         self.jobs = []
 
     # the function that does  the actual multiplying
-    def multiplier(self,beg,end):
+    def multiplier(self,beg,end,num):
         global result
+        #print("\tCalculating from {} to {}".format(beg,end)," : ",result)
         for i in range(beg,end+1):
-            result*= i
-            #print("\tresult updated with *{}:".format(i),result)
-        #print("Calculating from {} to {}".format(beg,end)," : ",result)
+            result = result * i % num
+            #print("\t\tresult updated with *{}:".format(i),result)
+        
     
     # appending threads to the object
     def append_job(self,job):
@@ -48,43 +53,92 @@ class Worker:
 def splitUp(n,t):
     # defining the remainder and the whole 
     remainder, whole = (n-1) % t, (n-1) // t
-    # deciding to tuple count
-    tuple_count = whole if remainder == 0 else whole + 1
     # empty result list 
     result = []
+    # deciding to tuple count
     # iterating 
     beginning = 1
-    end = (n-1) // t 
-    for i in range(1,tuple_count+1):
-        if i == tuple_count:
-            result.append((beginning,n-1)) # if we are at the end, just append all to end
+
+    if whole == 0:
+        tuple_count = n-1
+        for i in range(tuple_count):
+            result.append((beginning,beginning))
+            beginning += 1
+    else:
+        tuple_count = t
+        increaser = (n-1)//t + 1
+        if remainder == 0:
+            for i in range(tuple_count):
+                result.append((beginning,beginning+whole-1))
+                beginning = beginning + whole
         else:
-            result.append((beginning,end*i))
-        beginning = end*i + 1
+            j = 0
+            while j < tuple_count:
+                if j == tuple_count-1:
+                    result.append((beginning,n-1))
+                else: 
+                    result.append((beginning,beginning+increaser-2))
+                beginning = beginning + increaser -1
+                j += 1
+
+
+    print("### splitup result: ",result,remainder,whole)
     return result
 
+# reads the file and returns the num list
+def read_nums():
+    num_list = []
+    file_name = sys.argv[1]
+    with open(file_name,"r") as f:
+        for i in f.readlines():
+            if i[0] != "#":
+                num_list.append(int(i))
+    f.close()
+    print(num_list)
+    return num_list
+
+def write_results(string_list):
+    with open("result.txt","w") as f:
+        for s in string_list:
+            f.write(s)
+    f.close()
+
+def execution_per_num(number):
+    global threadlist,result
+    string_list = []
+    for num in number:
+        
+        for threads in threadlist:
+            print("Calculating for: ", str(num)," with thread count: ", str(threads))
+            splitted = splitUp(num,threads)
+            #creating worker object
+            worker = Worker()
+            #measuring time
+            s = time.time()
+            # creating the threads
+            for arg in splitted:
+                thread = threading.Thread(target=worker.multiplier(arg[0],arg[1],num))
+                worker.append_job(thread)
+
+            worker.initiate()
+            worker.work()
+            e = time.time()
+
+            
+            prime = result == num-1 # either 1 or zero 
+                
+            string = str(num) + ":\t" + ("NOT PRIME","PRIME")[prime] + " result found with {} threads in {} secs\n".format(threads,e-s)
+            string_list.append(string)
+            result = 1
+            print(("NOT PRIME","PRIME")[prime])
+        string_list.append("\n")
+    write_results(string_list)
+
 if __name__ == "__main__":
-    threads = 32
-    number = 743
-    splitted = splitUp(number,threads)
+    num_list = read_nums()
+    #num_list = [1,4,7,10]
+    execution_per_num(num_list)
 
-    worker = Worker()
-    #print(worker.see_jobs())
-
-    s = time.time()
-    # creating the threads
-    for arg in splitted:
-        thread = threading.Thread(target=worker.multiplier(arg[0],arg[1]))
-        worker.append_job(thread)
-
-    worker.initiate()
-    worker.work()
-    e = time.time()
-    print("result found with {} threads in {} secs\n".format(threads,e-s))
-    if result % number == number-1:
-        print("PRIME")
-    else:
-        print("NOT PRIME")
 
 """ 
 --------------------  REPORT  ------------------------
